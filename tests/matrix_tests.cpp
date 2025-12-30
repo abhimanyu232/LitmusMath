46c7ddfa3b4b7e1d4461cd81cbd8a2d3223d1a06
Abhimanyu_GLX <abhimanyu232@gmail.com>
2025-12-30 12:41:47 +0100

    added cStyle Matrix Multiplier function, revised CMakeLists, added CMakePresets, and starter github actions for CI/CD. Google tests linked but not implemented for any class.

--- tests/matrix_tests.cpp
+++ tests/matrix_tests.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 
 TEST(GoogleTestCi, Foo) {
+  EXPECT_TRUE(true);
+  ASSERT_TRUE(true);
   ASSERT_TRUE(true);
 }
 
 TEST(GoogleTestCii, Foo) {
-  ASSERT_TRUE(false);
+  ASSERT_TRUE(true);
 }
